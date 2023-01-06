4a18ca0ff3df590a48f05ab27c868e6a53a4a6e8
Adel <temp.openclassrooms@gmx.fr>
2023-01-06 15:31:27 +0100

    fin validation cart et récup id achat

--- front/js/cart.js
+++ front/js/cart.js
@@ -12,18 +12,19 @@ async function fetchrequest(id) {
 //récupération de la sélection pour rajouter dans le panier
 //---------------LOCAL STORAGE = LS--------------------------------
 //déclaration de la variable avec comme valeur la mémoire de LS
-const selectedProducts = JSON.parse(localStorage.getItem("purchaseInMemory"));
+let selectedProducts = JSON.parse(localStorage.getItem("purchaseInMemory"));
 //récupération de la balise html servant à l'insertion de l'achat
-const cart__items = document.getElementById("cart__items")
+let cart__items = document.getElementById("cart__items");
 //déclaration variable globale pour futur donnée LS
-let productApi = []
+let productApi = [];
 
 async function purchaseInsertion() {
   //création d'un fragement de code HTML invisible
   let fragment = document.createDocumentFragment();
   //boucle, pour chaque produit trouvé dans liste produits du LS
   for (product of selectedProducts) {
-    //variable qui récupère les informations dans l'API grace à l'ID du produit dans le LS
+    /*variable qui récupère les informations dans l'API
+    grace à l'ID du produit dans le LS*/
     productApi = await fetchrequest(product.id);
     //création de la balise article et ses attributs
     //product = info dans LS - productApi = info dans API
@@ -46,7 +47,8 @@ async function purchaseInsertion() {
       <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
           <p>Qté : </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${product.quantity}>
+          <input type="number" class="itemQuantity"
+          name="itemQuantity" min="1" max="100" value=${product.quantity}>
         </div>
         <div class="cart__item__content__settings__delete">
           <p class="deleteItem">Supprimer</p>
@@ -56,25 +58,27 @@ async function purchaseInsertion() {
     </article>`;
     //alimentation du fragement par l'ensemble des articles créés
     fragment.appendChild(article)
-  }
+  };
   //une fois que l'ensemble des articles sont dans le fragement
   //on insere le fragement dans le code HTML
   cart__items.appendChild(fragment)
 
-  //************SUPPRESSION PRODUIT************************************************
+  //************SUPPRESSION PRODUIT***********
   //boucle sur l'ensemble des boutons supprimer du code HTML
   document.querySelectorAll(".deleteItem").forEach(deleteItemButton => {
     const article = deleteItemButton.closest("article");
     //nous écoutons le bouton
-    deleteItemButton.addEventListener('click', function () {
+    deleteItemButton.addEventListener("click", function () {
       //on boucle sur l'ensemble des produits enregistrés dans le LS
       selectedProducts.forEach(product => {
         // quand le bouton supprimer correspond au produit dans le LS =>...
         if (product.id === article.getAttribute("data-id") &&
           product.color === article.getAttribute("data-color")) {
           //...=> nous le supprimons, mettons à jour le LS et on raffraichi
-          let delProduct = selectedProducts.filter(item => item.id != product.id) && selectedProducts.filter(item => item.color != product.color)
-          localStorage.setItem("purchaseInMemory", JSON.stringify(delProduct));
+          selectedProducts = selectedProducts.filter(item => item.id
+            != product.id)
+            && selectedProducts.filter(item => item.color != product.color)
+          localStorage.setItem("purchaseInMemory", JSON.stringify(selectedProducts));
           article.remove();
           updateTotal();
         }
@@ -82,15 +86,15 @@ async function purchaseInsertion() {
     })
   })
 
-  //************MODIFICATION QUANTITE************************************************
+  //************MODIFICATION QUANTITE*******
   //boucle sur l'ensemble des boutons quantité du code HTML
   document.querySelectorAll(".itemQuantity").forEach(itemQuantity => {
-    itemQuantity.addEventListener('change', function (event) {
+    itemQuantity.addEventListener("change", function (event) {
       //on boucle sur l'ensemble des produits enregistrés dans le LS
       selectedProducts.forEach(product => {
         // quand le bouton supprimer correspond au produit dans le LS =>...
-        if (product.id === itemQuantity.closest(".cart__item").getAttribute("data-id") &&
-          product.color === itemQuantity.closest(".cart__item").getAttribute("data-color")) {
+        if (product.id === itemQuantity.closest(".cart__item").getAttribute("data-id")
+          && product.color === itemQuantity.closest(".cart__item").getAttribute("data-color")) {
           //...=> nous le supprimons, mettons à jour le LS et on raffraichi
           if (itemQuantity.value > 100 || itemQuantity.value < 1) {
             alert("Veuillez choisir une quantité entre 1 et 100");
@@ -126,18 +130,6 @@ function updateTotal() {
 }
 
 //***********************FORMULAIRE CONTACT********************************************
-//fonction trouvée sur web afin de controler conformité email
-function ValidateEmail(inputText) {
-  const mailformat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  if (inputText.value.match(mailformat)) {
-    return true;
-  }
-  else {
-    alert("Vous avez saisi une adresse mail invalide !");    //The pop up alert for an invalid email address
-    document.getElementById("email").focus();
-    return false;
-  }
-}
 //récupération des info du formulaire dans un objet
 let formulaire = {
   firstName: document.getElementById("firstName").value,
@@ -152,8 +144,10 @@ les formulaires nom, prénom ville*/
 function containsNumbers(str, balise) {
   if (/\d/.test(str)) {
     // console.log(document.querySelector(str).textContent)
-    return document.querySelector(`#${balise}ErrorMsg`).textContent = "erreur"
-  };
+    return document.querySelector(`#${balise}ErrorMsg`).textContent = "Ne peut contenir que des lettres."
+  } else {
+    return document.querySelector(`#${balise}ErrorMsg`).textContent = ""
+  }
 }
 /*boucle sur les formulaires et controle uniquement
 les saisies nom, prénom et ville*/
@@ -163,19 +157,66 @@ for (const property in formulaire) {
     property === "city") {
     document.querySelector(`#${property}`).addEventListener("input", function () {
       containsNumbers(document.getElementById(property).value, property)
-      // document.querySelector(`#${property}ErrorMsg`).textContent = "erreur"
     })
   }
 }
 
-let order = document.getElementById("order")
-order.addEventListener("click", (event) => {
-  // console.log(document.querySelector("#firstName").closest(".cart__order__form__question #firstNameErrorMsg").innerHTML);
-  // itemQuantity.closest(".cart__item").getAttribute("data-color")
-
-  // ValidateEmail(email)
-  // localStorage.setItem("cartValidation", JSON.stringify(formulaire));
-
+//fonction trouvée sur web afin de controler conformité email
+function ValidateEmail(inputText) {
+  const mailformat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if (inputText.match(mailformat)) {
+    return document.querySelector("#emailErrorMsg").textContent = ""
+  }
+  else {
+    return document.querySelector("#emailErrorMsg").textContent = "Adresse mail non conforme"
+  }
+}
+//on test la conformité du mail en temps réél
+document.querySelector("#email").addEventListener("input", function () {
+  ValidateEmail(document.getElementById("email").value)
 })
 //*******************************************BOUTON COMMANDER**********************************
-let buttonOrder = document.getElementById("order")
+
+let order = document.getElementById("order")
+let products = [];
+
+// function standard qui appel l'API
+async function fetchrequestOrder(api) {
+  const fetchtReply = await api
+  if (fetchtReply.ok) {
+    return fetchtReply.json()
+    // console.log(fetchtReply.json())
+  } else {
+    console.log(fetchtReply.error)
+  }
+}
+
+order.addEventListener("click", (event) => {
+  event.preventDefault()
+  selectedProducts.forEach(product => {
+    products.push(product.id)
+  })
+
+  const sendCommand = {
+    contact: {
+      firstName: document.getElementById("firstName").value,
+      lastName: document.getElementById("lastName").value,
+      address: document.getElementById("address").value,
+      city: document.getElementById("city").value,
+      email: document.getElementById("email").value,
+    },
+    products: products
+  }
+  const sendCommandPromise = fetch("http://localhost:3000/api/products/order", {
+    method: "POST",
+    body: JSON.stringify(sendCommand),
+    headers: { "Content-Type": "application/json" },
+  })
+
+  async function displayProduct() {
+    const products = await fetchrequestOrder(sendCommandPromise);
+    console.log(products.orderId)
+    document.location.href = 'confirmation.html?id=' + products.orderId;
+  }
+  displayProduct()
+})
